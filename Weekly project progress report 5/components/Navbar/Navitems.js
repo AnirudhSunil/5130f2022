c3f1f752025d92d117ecba8932fbedc90ca22fee
AnirudhSunil <112992046+AnirudhSunil@users.noreply.github.com>
2022-12-04 14:56:44 -0500

    Create Navitems.js

---
+++ Weekly project progress report 5/components/Navbar/Navitems.js
@@ -0,0 +1,35 @@
+export const navItems = [
+  {
+    id: 1,
+    title: "Game Builds",
+    path: "./game_builds",
+    cName: "nav-item",
+  }
+];
+
+export const gbDropdown = [
+  {
+    id: 1,
+    title: "Call of Duty",
+    path: "./cod",
+    cName: "submenu-item",
+  },
+  {
+    id: 2,
+    title: "Fortnite",
+    path: "./fortnite",
+    cName: "submenu-item",
+  },
+  {
+    id: 3,
+    title: "Grand Theft Auto V",
+    path: "/gta",
+    cName: "submenu-item",
+  },
+  {
+    id: 4,
+    title: "League of Legends",
+    path: "./lol",
+    cName: "submenu-item",
+  },
+];
